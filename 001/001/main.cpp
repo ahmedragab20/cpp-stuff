fb842aa334495a2db295084801858275d2957f47
ahmedragab <ahmedragab20901@gmail.com>
2023-09-15 23:33:45 +0300

    first cpp hello world

---
+++ 001/001/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  001
+//
+//  Created by Ahmed Ragab on 15/09/2023.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // insert code here...
+    cout << "Hello World :)\n" << endl;
+    return 0;
+}
