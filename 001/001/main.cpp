09574c3c9d90c74fc0b498c6fa7416e44d4f5be8
ahmedragab <ahmedragab20901@gmail.com>
2023-09-17 06:55:10 +0300

    update

--- 001/001/main.cpp
+++ 001/001/main.cpp
@@ -5,14 +5,21 @@
 //  Created by Ahmed Ragab on 15/09/2023.
 //
 
+/*
+ argc -> arguments count
+ *argv -> arguments victor
+ :: -> scope resolution operator
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    int fav_number;
-    cout << "Enter your favourite number: ";
-    cin >> fav_number;
+    int fav_number; // favorite number
+    cout << "Enter your favourite number: "; // insertion operator
+    cin >> fav_number; // extraction operator
     cout <<  fav_number << " is a very good number!" << endl;
+    
     return 0;
 }
